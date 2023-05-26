dc570ba230415a7c598c0d36805ea9ec02ce9514
Steph Beneschan <36548653+steph-beneschan-256@users.noreply.github.com>
2023-05-26 15:35:14 -0700

    last minute bug fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,20 @@ import { useState } from 'react';
 
 function App() {
 
+  const[inputPortfolio, setInputPortfolio] = useState(null);
   const [portfolio, setPortfolio] = useState(null);
+  const rawStockData = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [chartLoaded, setChartLoaded] = useState(false);
+  const [chartSeries, setChartSeries] = useState(null);
+  const [finalTotalValue, setFinalTotalValue] = useState(0);
+  const [finalStockValues, setFinalStockValues] = useState(new Map());
+  const [errMsg, setErrMsg] = useState("");
+
+  function updateInputPortfolio(newPortfolio) {
+    setInputPortfolio(newPortfolio);
+  }
 
   // Call when Calculate Results button is clicked
   function calculatePortfolioResults(newPortfolio) {
@@ -16,10 +29,225 @@ function App() {
       if(resultsDiv){
         resultsDiv.scrollIntoView();
       }
+      //getRawStockData(newPortfolio);
+      updateChart(newPortfolio);
     }
 
 
   }
+
+  /*
+  For a given Date object, create a string in the format
+  YYYY-MM-DD,
+  to supply to the Marketstack or Twelvedata APIs
+  */
+  function formatDateStr(date) {
+    const y = date.getUTCFullYear().toString().padStart(4,'0');
+    const m = date.getUTCMonth().toString().padStart(2, '0');
+    const d = date.getUTCDate().toString().padStart(2, '0');
+    return `${y}-${m}-${d}`;
+  }
+
+  
+  /*
+  Get stock data from the TwelveData API
+
+  NOTE: The Twelvedata support website indicates that all interday data is adjusted for splits:
+  https://support.twelvedata.com/en/articles/5179064-are-the-prices-adjusted
+  */
+  async function getRawStockData(newPortfolio) {
+
+    // if(portfolio !== savedPortfolio.current) {
+    //   savedPortfolio.current = portfolio;
+    // }
+    // else if(rawStockData.current)
+    //   return rawStockData.current;
+
+    // const sampleData = require('./twelve-data-sample-data.json');
+    // rawStockData.current = sampleData;
+    // return sampleData;
+
+    const symbols = newPortfolio.assets.map((a) => a.symbol);
+
+    //const apiKey = "";
+    const apiKey = "8bc49cd65a834ef9b55a2110f3a2529c";
+    const requestURL = "https://api.twelvedata.com/time_series?"
+    + new URLSearchParams({
+      apikey: apiKey,
+      symbol: symbols.join(','),
+      interval: "1month",
+      start_date: formatDateStr(new Date(newPortfolio.startDate)),
+      end_date: formatDateStr(new Date()),
+      outputsize: 5000
+      // sort: "ASC" // Seems to not work for some reason
+    });
+
+    const reqResponse = await fetch(requestURL);
+    if(reqResponse.status === 200) {
+      const jsonData = await reqResponse.json();
+      if(symbols.length === 1) {
+        let toReturn = {};
+        toReturn[symbols[0]] = jsonData;
+        rawStockData.current = toReturn;
+        return toReturn;
+      }
+      rawStockData.current = jsonData;
+      console.log(jsonData);
+      return jsonData;
+    }
+    else {
+      //setErrMsg("Sorry, but something went wrong. Please try again in a few moments.");
+      return null;
+    }
+
+  }
+
+  async function updateChart(newPortfolio) {
+    setIsLoading(true);
+
+    const stockData = await getRawStockData(newPortfolio);
+    // if(stockData === null) {
+    //     setErrMsg("Sorry, but we're having trouble accessing stock data. Please try again in a few moments.")
+    //     setIsLoading(false);
+    //     return;
+    // }
+
+    if(!stockData) {
+      //error
+      setIsLoading(false);
+      return;
+    }
+
+    console.log(stockData);
+
+    const stockSymbols = newPortfolio.assets.map((asset) => asset.symbol);
+    console.log(stockSymbols);
+
+    /*
+    Ensure that data for all stocks in the portfolio was successfully retrieved
+    */
+    let e = false;
+    stockSymbols.forEach((symbol) => {
+      if((!stockData[symbol]) || (stockData[symbol].status !== "ok")) {
+        setErrMsg("Sorry, but data could not be retrieved for every stock in your portfolio. Please make sure that you entered valid stock symbols.");
+        e = true;
+        return;
+      }
+    })
+    if(e){
+        setIsLoading(false);
+        return;
+    }
+
+    /*
+    Get the number of shares purchased for each stock in the portfolio
+    */
+    const sharesBought = new Map();
+    newPortfolio.assets.forEach((asset) => {
+      const assetData = stockData[asset.symbol].values;
+      const initialPrice = assetData[assetData.length - 1].close; // assume descending order
+      const amountInvested = asset.portion * newPortfolio.initial;
+      sharesBought.set(asset.symbol, amountInvested / initialPrice);
+    });
+
+    //should we assume that if information on a given date is available for one stock, information will be available for every stock on that date?
+
+    /*
+    For each stock, record its value over time, in a format
+    that can be passed to the chart thing
+
+    maybe ignore total for now???
+    */
+
+    let newChartSeries = [];
+
+    /*
+    For every date on which data is available for at least one stock, record what stocks changed on that day and what their new values were afterwards. (This will be used to find the total portfolio's value over time.)
+    */
+    const valuesByDate = new Map();
+
+    stockSymbols.forEach((symbol) => {
+      newChartSeries.push(
+        {
+          name: symbol,
+          type: "area",
+          data: 
+            stockData[symbol]["values"].map((tradingDay) => {
+              const date = tradingDay.datetime;
+              const closingPrice = tradingDay.close;
+              const assetValue = sharesBought.get(symbol) * closingPrice;
+
+              const v = [[symbol, assetValue]];
+              if(!valuesByDate.has(date))
+                valuesByDate.set(date, v);
+              else
+                valuesByDate.set(date, valuesByDate.get(date).concat(v));
+    
+              return {
+                x: date,
+                y: assetValue.toFixed(2)
+              };
+            })
+          
+        }
+      )
+    });
+
+    // Estimate total portfolio value over time
+    const totalValueSeries = [];
+
+    // Convert date strings to date objects, to enable sorting
+    const allDates = Array.from(valuesByDate.keys());
+    allDates.sort((a,b) => (new Date(a) - new Date(b)));
+
+    /*
+      If a particular stock's price isn't available for a given day, assume that
+      its price was the same as its previous price, for the purposes of calculating
+      the total portfolio value for that day.
+    */
+    const currentValues = new Map();
+    newPortfolio.assets.forEach((asset) => {
+      currentValues.set(asset.symbol, 0);
+    })
+    let portfolioValue = newPortfolio.initial;
+    allDates.forEach((date) => {
+      valuesByDate.get(date).forEach(([symbol, newValue]) => {
+        currentValues.set(symbol, newValue);
+      })
+      portfolioValue = Array.from(currentValues.values()).reduce(
+        (accumulator, currValue) => (accumulator + currValue), 0);
+      
+      totalValueSeries.push({
+        x: date,
+        y: portfolioValue
+      });
+  
+    })
+    
+    newChartSeries.push({
+      name: "Total",
+      type: "area",
+      data: totalValueSeries
+    });
+
+    // Finally, update states
+
+    setChartSeries(newChartSeries);
+    setFinalStockValues(stockSymbols.map((symbol) => {
+      return {
+        symbol: symbol,
+        value: currentValues.get(symbol)
+      }
+    }));
+    setFinalTotalValue(portfolioValue);
+    setChartLoaded(true);
+    setIsLoading(false);
+
+    console.log(newChartSeries);
+
+  }
+
+
 
   return (
     <div className="App">
@@ -30,7 +258,7 @@ function App() {
         <PortfolioInput onPortfolioSubmit={calculatePortfolioResults}/>
         <div id="results">
           {portfolio &&
-          <ShowResults portfolio={portfolio} />}
+          <ShowResults portfolio={portfolio} stockData={rawStockData.current} finalTotalValue={finalTotalValue} finalStockValues={finalStockValues} chartSeries={chartSeries} />}
         </div>
       </body>
     </div>
--- src/ShowResults.js
+++ src/ShowResults.js
@@ -57,12 +57,12 @@ const samplePortfolio =
 /*
 The ShowResults component
 */
-export default function ShowResults ({ portfolio=samplePortfolio }) {
+export default function ShowResults ({ portfolio=samplePortfolio, stockData, finalTotalValue, finalStockValues, chartSeries }) {
   const [isLoading, setIsLoading] = useState(false);
   const [chartLoaded, setChartLoaded] = useState(false);
-  const [chartSeries, setChartSeries] = useState(null);
-  const [finalTotalValue, setFinalTotalValue] = useState(0);
-  const [finalStockValues, setFinalStockValues] = useState(new Map());
+  //const [chartSeries, setChartSeries] = useState(null);
+  //const [finalTotalValue, setFinalTotalValue] = useState(0);
+  //const [finalStockValues, setFinalStockValues] = useState(new Map());
   const [errMsg, setErrMsg] = useState("");
 
   const savedPortfolio = useRef(null);
@@ -90,215 +90,145 @@ export default function ShowResults ({ portfolio=samplePortfolio }) {
     },
   };
 
-  /*
-  For a given Date object, create a string in the format
-  YYYY-MM-DD,
-  to supply to the Marketstack or Twelvedata APIs
-  */
-  function formatDateStr(date) {
-    const y = date.getUTCFullYear().toString().padStart(4,'0');
-    const m = date.getUTCMonth().toString().padStart(2, '0');
-    const d = date.getUTCDate().toString().padStart(2, '0');
-    return `${y}-${m}-${d}`;
-  }
+  // /*
+  // Update the chart, using the raw stock data
+  // */
+  // async function updateChart() {
+  //   setIsLoading(true);
 
-  /*
-  Get stock data from the TwelveData API
+  //   // const stockData = await getRawStockData();
+  //   // if(stockData === null) {
+  //   //     setErrMsg("Sorry, but we're having trouble accessing stock data. Please try again in a few moments.")
+  //   //     setIsLoading(false);
+  //   //     return;
+  //   // }
 
-  NOTE: The Twelvedata support website indicates that all interday data is adjusted for splits:
-  https://support.twelvedata.com/en/articles/5179064-are-the-prices-adjusted
-  */
-  async function getRawStockData() {
+  //   console.log(stockData);
 
-    if(rawStockData.current)
-      return rawStockData.current;
+  //   const stockSymbols = portfolio.assets.map((asset) => asset.symbol);
+  //   console.log(stockSymbols);
 
-    // let useSampleData = true;
-    // useSampleData = false;
+  //   /*
+  //   Ensure that data for all stocks in the portfolio was successfully retrieved
+  //   */
+  //   let e = false;
+  //   stockSymbols.forEach((symbol) => {
+  //     if((!stockData[symbol]) || (stockData[symbol].status !== "ok")) {
+  //       setErrMsg("Sorry, but data could not be retrieved for every stock in your portfolio. Please make sure that you entered valid stock symbols.");
+  //       e = true;
+  //       return;
+  //     }
+  //   })
+  //   if(e){
+  //       setIsLoading(false);
+  //       return;
+  //   }
 
-    // if(useSampleData) {
-    //   rawStockData.current = sampleData;
-    //   return sampleData;
-    // }
+  //   /*
+  //   Get the number of shares purchased for each stock in the portfolio
+  //   */
+  //   const sharesBought = new Map();
+  //   portfolio.assets.forEach((asset) => {
+  //     const assetData = stockData[asset.symbol].values;
+  //     const initialPrice = assetData[assetData.length - 1].close; // assume descending order
+  //     const amountInvested = asset.portion * portfolio.initial;
+  //     sharesBought.set(asset.symbol, amountInvested / initialPrice);
+  //   });
 
-    const symbols = portfolio.assets.map((a) => a.symbol);
+  //   //should we assume that if information on a given date is available for one stock, information will be available for every stock on that date?
 
-    //const apiKey = "";
-    const apiKey = "8bc49cd65a834ef9b55a2110f3a2529c";
-    const requestURL = "https://api.twelvedata.com/time_series?"
-    + new URLSearchParams({
-      apikey: apiKey,
-      symbol: symbols.join(','),
-      interval: "1month",
-      start_date: formatDateStr(new Date(portfolio.startDate)),
-      end_date: formatDateStr(new Date()),
-      outputsize: 5000
-      // sort: "ASC" // Seems to not work for some reason
-    });
+  //   /*
+  //   For each stock, record its value over time, in a format
+  //   that can be passed to the chart thing
 
-    const reqResponse = await fetch(requestURL);
-    if(reqResponse.status === 200) {
-      const jsonData = await reqResponse.json();
-      if(symbols.length === 1) {
-        let toReturn = {};
-        toReturn[symbols[0]] = jsonData;
-        rawStockData.current = toReturn;
-        return toReturn;
-      }
-      rawStockData.current = jsonData;
-      console.log(jsonData);
-      return jsonData;
-    }
-    else {
-      setErrMsg("Sorry, but something went wrong.");
-      return null;
-    }
+  //   maybe ignore total for now???
+  //   */
 
-  }
+  //   let newChartSeries = [];
 
-  /*
-  Update the chart, using the raw stock data
-  */
-  async function updateChart() {
-    setIsLoading(true);
+  //   /*
+  //   For every date on which data is available for at least one stock, record what stocks changed on that day and what their new values were afterwards. (This will be used to find the total portfolio's value over time.)
+  //   */
+  //   const valuesByDate = new Map();
 
-    const stockData = await getRawStockData();
-    if(stockData === null) {
-        setErrMsg("Sorry, but we're having trouble accessing stock data. Please try again in a few moments.")
-        setIsLoading(false);
-        return;
-    }
+  //   stockSymbols.forEach((symbol) => {
+  //     newChartSeries.push(
+  //       {
+  //         name: symbol,
+  //         type: "area",
+  //         data: 
+  //           stockData[symbol]["values"].map((tradingDay) => {
+  //             const date = tradingDay.datetime;
+  //             const closingPrice = tradingDay.close;
+  //             const assetValue = sharesBought.get(symbol) * closingPrice;
 
-    const stockSymbols = portfolio.assets.map((asset) => asset.symbol);
-    console.log(stockSymbols);
-
-    /*
-    Ensure that data for all stocks in the portfolio was successfully retrieved
-    */
-    let e = false;
-    stockSymbols.forEach((symbol) => {
-      if((!stockData[symbol]) || (stockData[symbol].status !== "ok")) {
-        setErrMsg("Sorry, but data could not be retrieved for every stock in your portfolio. Please make sure that you entered valid stock symbols.");
-        e = true;
-        return;
-      }
-    })
-    if(e){
-        setIsLoading(false);
-        return;
-    }
-
-    /*
-    Get the number of shares purchased for each stock in the portfolio
-    */
-    const sharesBought = new Map();
-    portfolio.assets.forEach((asset) => {
-      const assetData = stockData[asset.symbol].values;
-      const initialPrice = assetData[assetData.length - 1].close; // assume descending order
-      const amountInvested = asset.portion * portfolio.initial;
-      sharesBought.set(asset.symbol, amountInvested / initialPrice);
-    });
-
-    //should we assume that if information on a given date is available for one stock, information will be available for every stock on that date?
-
-    /*
-    For each stock, record its value over time, in a format
-    that can be passed to the chart thing
-
-    maybe ignore total for now???
-    */
-
-    let newChartSeries = [];
-
-    /*
-    For every date on which data is available for at least one stock, record what stocks changed on that day and what their new values were afterwards. (This will be used to find the total portfolio's value over time.)
-    */
-    const valuesByDate = new Map();
-
-    stockSymbols.forEach((symbol) => {
-      newChartSeries.push(
-        {
-          name: symbol,
-          type: "area",
-          data: 
-            stockData[symbol]["values"].map((tradingDay) => {
-              const date = tradingDay.datetime;
-              const closingPrice = tradingDay.close;
-              const assetValue = sharesBought.get(symbol) * closingPrice;
-
-              const v = [[symbol, assetValue]];
-              if(!valuesByDate.has(date))
-                valuesByDate.set(date, v);
-              else
-                valuesByDate.set(date, valuesByDate.get(date).concat(v));
+  //             const v = [[symbol, assetValue]];
+  //             if(!valuesByDate.has(date))
+  //               valuesByDate.set(date, v);
+  //             else
+  //               valuesByDate.set(date, valuesByDate.get(date).concat(v));
     
-              return {
-                x: date,
-                y: assetValue.toFixed(2)
-              };
-            })
+  //             return {
+  //               x: date,
+  //               y: assetValue.toFixed(2)
+  //             };
+  //           })
           
-        }
-      )
-    });
+  //       }
+  //     )
+  //   });
 
-    // Estimate total portfolio value over time
-    const totalValueSeries = [];
+  //   // Estimate total portfolio value over time
+  //   const totalValueSeries = [];
 
-    // Convert date strings to date objects, to enable sorting
-    const allDates = Array.from(valuesByDate.keys());
-    allDates.sort((a,b) => (new Date(a) - new Date(b)));
+  //   // Convert date strings to date objects, to enable sorting
+  //   const allDates = Array.from(valuesByDate.keys());
+  //   allDates.sort((a,b) => (new Date(a) - new Date(b)));
 
-    /*
-      If a particular stock's price isn't available for a given day, assume that
-      its price was the same as its previous price, for the purposes of calculating
-      the total portfolio value for that day.
-    */
-    const currentValues = new Map();
-    portfolio.assets.forEach((asset) => {
-      currentValues.set(asset.symbol, 0);
-    })
-    let portfolioValue = portfolio.initial;
-    allDates.forEach((date) => {
-      valuesByDate.get(date).forEach(([symbol, newValue]) => {
-        currentValues.set(symbol, newValue);
-      })
-      portfolioValue = Array.from(currentValues.values()).reduce(
-        (accumulator, currValue) => (accumulator + currValue), 0);
+  //   /*
+  //     If a particular stock's price isn't available for a given day, assume that
+  //     its price was the same as its previous price, for the purposes of calculating
+  //     the total portfolio value for that day.
+  //   */
+  //   const currentValues = new Map();
+  //   portfolio.assets.forEach((asset) => {
+  //     currentValues.set(asset.symbol, 0);
+  //   })
+  //   let portfolioValue = portfolio.initial;
+  //   allDates.forEach((date) => {
+  //     valuesByDate.get(date).forEach(([symbol, newValue]) => {
+  //       currentValues.set(symbol, newValue);
+  //     })
+  //     portfolioValue = Array.from(currentValues.values()).reduce(
+  //       (accumulator, currValue) => (accumulator + currValue), 0);
       
-      totalValueSeries.push({
-        x: date,
-        y: portfolioValue
-      });
+  //     totalValueSeries.push({
+  //       x: date,
+  //       y: portfolioValue
+  //     });
   
-    })
+  //   })
     
-    newChartSeries.push({
-      name: "Total",
-      type: "area",
-      data: totalValueSeries
-    });
+  //   newChartSeries.push({
+  //     name: "Total",
+  //     type: "area",
+  //     data: totalValueSeries
+  //   });
 
-    // Finally, update states
+  //   // Finally, update states
 
-    setChartSeries(newChartSeries);
-    setFinalStockValues(stockSymbols.map((symbol) => {
-      return {
-        symbol: symbol,
-        value: currentValues.get(symbol)
-      }
-    }));
-    setFinalTotalValue(portfolioValue);
-    setChartLoaded(true);
-    setIsLoading(false);
+  //   setChartSeries(newChartSeries);
+  //   setFinalStockValues(stockSymbols.map((symbol) => {
+  //     return {
+  //       symbol: symbol,
+  //       value: currentValues.get(symbol)
+  //     }
+  //   }));
+  //   setFinalTotalValue(portfolioValue);
+  //   setChartLoaded(true);
+  //   setIsLoading(false);
 
-  }
-
-  if(portfolio !== savedPortfolio.current) {
-    savedPortfolio.current = portfolio;
-    updateChart();
-  }
-
+  // }
 
   return (
     <div className="showResults">
@@ -318,7 +248,7 @@ export default function ShowResults ({ portfolio=samplePortfolio }) {
         </div>
 
         <>  
-          {(chartLoaded && chartOptions && chartSeries) && 
+          {(chartOptions && chartSeries) && 
             <>
             <div className="resultsChartContainer">
               <Chart
